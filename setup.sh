32ca37884f7aa4ad3a6164ba60d87b29313445d6
root <root@vultr.guest>
2016-09-08 20:13:47 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-yum install git zlib-devel openssl-devel
+echo -n "enter your shadowsocks server port: "  
+read  shadowsocksport 
+echo -n "enter your shadowsocks server password: "  
+read  shadowsockspwd
 
+yum groupinstall "Development Tools"
+yum install git zlib-devel openssl-devel pcre-devel
+
+# Make sure only root can run our script
+function rootness(){
+if [[ $EUID -ne 0 ]]; then
+   echo "Error:This script must be run as root!" 1>&2
+   exit 1
+fi
+}
+
+#install serverspeeder
 #wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/code/master/vm_check.sh && bash vm_check.sh
-rpm -ivh http://soft.91yun.org/ISO/Linux/CentOS/kernel/kernel-firmware-2.6.32-504.3.3.el6.noarch.rpm
-rpm -ivh http://soft.91yun.org/ISO/Linux/CentOS/kernel/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
-wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh
+#rpm -ivh http://soft.91yun.org/ISO/Linux/CentOS/kernel/kernel-firmware-2.6.32-504.3.3.el6.noarch.rpm
+#rpm -ivh http://soft.91yun.org/ISO/Linux/CentOS/kernel/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
+#wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh
+
 
 
 git clone https://github.com/shadowsocks/shadowsocks-libev.git
@@ -14,9 +31,20 @@ cd shadowsocks-libev
 make 
 make install
 
-mkdir /etc/shadowsocks-libev
-copy config.json /etc/shadowsocks-libev/config.json
-copy shadowsocks-libev /etc/init.d/shadowsocks-libev
+if [ ! -d /etc/shadowsocks-libev ];then
+        mkdir /etc/shadowsocks-libev
+    fi
+    cat > /etc/shadowsocks-libev/config.json<<-EOF
+{
+    "server":"0.0.0.0",
+    "server_port":${shadowsocksport},
+    "password":"${shadowsockspwd}",
+    "timeout":600,
+    "method":"aes-128-cfb"
+}
+EOF
+
+cp ${DIR}/templatefiles/shadowsocks-libev /etc/init.d/shadowsocks-libev
 chmod u+x /etc/init.d/shadowsocks-libev
 /sbin/chkconfig --add shadowsocks-libev
 /etc/init.d/shadowsocks-libev start
--- wordpress/wordpress.sh
+++ wordpress/wordpress.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
+DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
-yum install nginx,php-ftp,php,php-sqlite3
-yum install unzip
+yum install php-fpm php php-sqlite3 unzip
 
-cp ./default.conf /etc/nginx/conf.d/default.conf
+if [ ! -f /etc/nginx/conf.d/default.conf ]; then
+	yum install nginx
+	cp ${DIR}/default.conf /etc/nginx/conf.d/default.conf
+        cp ${DIR}/www.conf /etc/php-fpm.d/www.conf
+fi
 
-cd /var/www
-wget http://wordpress.org/latest.zip
-unzip latest.zip
-cd wordpress/wp-content/plugins
-wget https://downloads.wordpress.org/plugin/sqlite-integration.1.8.1.zip
-unzip sqlite-integration.1.8.1.zip
-cp sqlite-integration/db.php ../db.php
-cd /var/www/wordpress
-cp wp-config-sample.php wp-config.php
-chown -R nginx:nginx wordpress
+if [ ! -d /var/www/wordpress ]; then
+	cd /var/www
+	wget http://wordpress.org/latest.zip
+	echo 'unziping wordpress'
+	unzip latest.zip > /dev/null
+	cd /var/www/wordpress
+	cp wp-config-sample.php wp-config.php
+	chown -R nginx:nginx wordpress
+	cp ${DIR}/default.conf /etc/nginx/conf.d/default.conf
+fi
 
-mkdir /var/wordpressdata
-chown -R nginx:nginx /var/wordpressdata
+if [ ! -d /var/www/wordpress/wp-content/plugins/sqlite-integration ]; then
+	cd /var/www/wordpress/wp-content/plugins
+	wget https://downloads.wordpress.org/plugin/sqlite-integration.1.8.1.zip
+	echo 'unziping sqlite-integration'
+	unzip sqlite-integration.1.8.1.zip > /dev/null
+	cp sqlite-integration/db.php ../db.php
+fi
 
+if [ ! -d /var/wordpressdata ]; then
+	mkdir /var/wordpressdata
+	chown -R nginx:nginx /var/wordpressdata
+fi
+
+iptables -I INPUT -m state --state NEW -m tcp -p tcp  --dport 80 -j ACCEPT
+
+/etc/init.d/iptables save
+/etc/init.d/iptables restart
